a3e564ea56f3987b881eb6d82ed436fadb2015d1
azmianshari <azmianshari123@gmail.com>
2023-05-08 15:38:04 +0700

    Membuat int main Friendclass

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -11,3 +11,33 @@ public:
 	friend class siswa;
 };
 
+class siswa {
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
+
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+int main() {
+	orang o;
+	o.setNama("Joko Kumat");
+	siswa s;
+	s.setId(1);
+	s.displayAll(o);
+	cout << endl;
+	system("Pause");
+	return 0;
+}
